e547fd99d63ac45846d7d9087c826fbaaaeee10c
Pablo-source <datavizvalencia@gmail.com>
2025-09-27 17:15:56 +0100

    using gridExtra to combine percent change house price charts

--- 02_House_data_EDA.R
+++ 02_House_data_EDA.R
@@ -5,8 +5,8 @@ source('01_Load_house_prices_data.R')
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
-# 1 Start exploring "Average_price" data set: 
 
+# 1 Start exploring "Average_price" data set: 
 head(Average_house_price)
 
 # 1-2 Annual Price change 
@@ -21,7 +21,6 @@ Price_changed <- Price_change %>%
                   mutate(datef = as.Date(date, format = "%Y/%b/%d"))
 Price_changed
 
-
 Price_changed <- Price_changed %>% select(datef,Chnge_price = Price_change)
 Price_changed
 
@@ -33,6 +32,7 @@ CHG_HPRICE <-ggplot(data = Price_changed, aes( x = datef, y = Chnge_price)) +
        x = "Date", y ="Price change %" ) +
   scale_y_continuous(breaks = seq(-20,20, by = 2)) +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  geom_hline(yintercept = 0, linewidth = 0.3)  +        # Add reference line at 0
   theme_bw()
 CHG_HPRICE
 
@@ -64,7 +64,7 @@ tail(Average_priced)
 
 # Plot average house prices
 AVG_HPRICE <-ggplot(data = Average_priced, aes( x = datef, y = Avg_price)) + 
-  geom_line(color="dodgerblue") +
+  geom_line(color="orange") +
   labs(title ="UK Average house price in England 2005-2023. Latest data December 2023",
        subtitle = "Average house price in pounds",
        x = "Date", y ="House price (£)") +
@@ -74,4 +74,37 @@ AVG_HPRICE <-ggplot(data = Average_priced, aes( x = datef, y = Avg_price)) +
 AVG_HPRICE
 
 ggsave(paste0("plots/Average_house_price_in_pounds.jpeg"),width = 30, height = 20, dpi = 150, units = "cm")
+
+# 3. We can place both of the above charts on the same output chart using {gridExtra} package
+install.packages("gridExtra",dependencies = TRUE)
+library(gridExtra)
+
+# 3.1 Combine previous two charts for "UK Average House Price" and "UK Annual house price change" between Jan 2025 and Dec 2025
+
+# 2.1 UK Average house price change
+
+CHG_HPRICE_grid <-ggplot(data = Price_changed, aes( x = datef, y = Chnge_price)) + 
+  geom_line(color="royalblue4") +
+  labs(title ="UK Annual house price change in England 2005-2023. Latest data December 2023",
+       subtitle = "% Annual change UK House pricess-Nationawide",
+       x = "Date", y ="Price change %" ) +
+  scale_y_continuous(breaks = seq(-20,20, by = 2)) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  geom_hline(yintercept = 0, linewidth = 0.3)  +        # Add reference line at 0
+  theme_bw()
+CHG_HPRICE_grid
+
+# 2-2. Average House price data 
+AVG_HPRICE_grid <-ggplot(data = Average_priced, aes( x = datef, y = Avg_price)) + 
+  geom_line(color="orange") +
+  labs(title ="UK Average house price in England 2005-2023. Latest data December 2023",
+       subtitle = "Average house price in pounds",
+       x = "Date", y ="House price (£)") +
+  scale_y_continuous(breaks = seq(150000,400000, by = 10000)) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme_bw()
+AVG_HPRICE_grid
+
+grid.arrange(CPI_chart, CPIH_chart, OOH_chart, ncol=3)
+ggsave("plots/32_Inflation_grid_April_2023.png", width = 6, height = 4) 
 
