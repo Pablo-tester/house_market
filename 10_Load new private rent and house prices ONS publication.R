36eb1f72d38a642615f5736c76755bfe1dcc54f3
Pablo-source <datavizvalencia@gmail.com>
2025-09-29 08:24:58 +0100

    Started working on 10_load_new_private rent and house price ONS publication script

--- 10_Load new private rent and house prices ONS publication.R
+++ 10_Load new private rent and house prices ONS publication.R
@@ -3,6 +3,7 @@
 #  - Private rent and house prices, UK: September 2025. (This is the latest release):
 #  https://www.ons.gov.uk/economy/inflationandpriceindices/bulletins/privaterentandhousepricesuk/september2025
 
-
 # Downloaded Excel file (.xls) from Figure 1: UK private rent and house prices
+
 # 1. Load private rents index and house price index
+# It is located inside \data_UK_House_Price_Index folder 
